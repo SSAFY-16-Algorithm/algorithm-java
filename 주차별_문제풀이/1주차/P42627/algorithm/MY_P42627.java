dea7ef71c83700c2ab5b5e17b83f8d4cbfa867a9
IENFI <myh020118@gmail.com>
2026-08-05 01:56:39 +0900

    하민영: P42627 디스크 컨트롤러
    * 작업 소요시간, 요청 시각, 작업 번호를 기준으로 PriorityQueue 우선순위 설정
    * 처음에는 모든 작업을 큐에 넣어 아직 요청되지 않은 작업까지 선택되는 문제 발생
    * 현재 시각까지 요청된 작업만 큐에 추가하도록 로직 수정
    * 실행 가능한 작업이 없을 경우 다음 요청 시각으로 현재 시간 이동
    * Comparator 체이닝과 PriorityQueue 활용 방법 학습

---
+++ 주차별_문제풀이/1주차/P42627/algorithm/MY_P42627.java
@@ -0,0 +1,75 @@
+package algorithm;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
+public class MY_P42627 {
+    public static int solution(int[][] jobs) {
+    	int workSize = jobs.length;
+    	Work[] works = new Work[workSize];
+    	PriorityQueue<Work> pq = new PriorityQueue<>(
+			Comparator.comparingInt((Work work) -> work.duration)
+				.thenComparingInt(w -> w.reqTime)
+				.thenComparingInt(w -> w.num)
+		);
+    	
+    	// 바로 작업 큐에 넣으면
+    	// 3ms에 (5ms, 1)과 (0ms, 1)이 있는 상황에
+    	// 없어야할 5ms 시작 작업을 꺼내게된다.
+    	// pq의 정렬 때문에
+    	// 그래서 작업 시간에 맞춰 while문에서 직접 큐에 넣어야 함
+    	for (int i = 0; i < workSize; i++) {
+    		works[i] = new Work(i, jobs[i][0], jobs[i][1]);
+    	}
+    	
+    	// 요청시간, 작업 번호 순으로 정렬
+    	Arrays.sort(works, 
+			Comparator.comparingInt((Work work) -> work.reqTime)
+				.thenComparingInt(work -> work.num)
+		);
+    	
+    	int now = 0; // 현재 시간
+    	int turnaroundTime = 0;
+    	int idx = 0;
+    	
+    	// 모든 작업을 큐에 넣었음에도
+    	// 더 이상 할 일이 없을 때 종료
+    	while(idx < workSize || !pq.isEmpty()) {
+    		// 현재 시간에 요청된 작업 큐에 추가
+    		while(idx < workSize && works[idx].reqTime <= now) {
+    			pq.add(works[idx]);
+    			idx++;
+    		}
+    		
+    		if (pq.isEmpty()) {
+    			now++;
+    			continue;
+    		}
+    		
+    		Work work = pq.poll();
+    		now += work.duration;
+    		turnaroundTime += (now - work.reqTime);
+    	}
+    	
+        int answer = (int) turnaroundTime/workSize;
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		System.out.println(solution(new int[][] {{0, 3}, {1, 9}, {3, 5}}));
+
+	}
+
+
+	public static class Work {
+		int num;
+		int reqTime;
+		int duration;
+		Work(int n, int r, int d) {
+			this.num = n;
+			this.reqTime = r;
+			this.duration = d;
+		}
+	}
+}
