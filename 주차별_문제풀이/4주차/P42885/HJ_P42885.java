1ac2c00fdb394f947d293c83f53377eaa218aeae
BaeHyungJoon <bhj01073206033@gmail.com>
2026-09-02 00:51:02 +0900

    배형준 P42885 구명보트

---
+++ 주차별_문제풀이/4주차/P42885/HJ_P42885.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+	
+    public int solution(int[] people, int limit) {
+    	
+    	Arrays.sort(people);
+    	
+    	int left = 0;
+    	int right = people.length - 1;
+    	int boat = 0;
+    	
+    	while (left < right) {
+    		if (people[left] + people[right] <= limit) {
+    			left++;
+    		}
+    		right--;
+    		boat++;
+    	}
+    	
+    	if (left == right) {
+    		boat++;
+    	}
+    	
+    	
+        return boat;
+        
+    }
+    
+}
