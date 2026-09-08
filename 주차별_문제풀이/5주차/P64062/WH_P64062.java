e5c6868081bb4300c04152fcb04403982b9a0a54
wonandonly <102897324+wonandonly@users.noreply.github.com>
2026-09-08 16:24:23 +0900

    김원희: P64062 징검다리 건너기
    Refactor solution method and add canPass method for efficiency

--- 주차별_문제풀이/5주차/P64062/WH_P64062.java
+++ 주차별_문제풀이/5주차/P64062/WH_P64062.java
@@ -1,77 +1,40 @@
-//수정했는데 정확성 테스트 다 통과했는데 효율성 테스트 다 틀림 아ㅠㅠ
-
-import java.util.*;
 class Solution {
-    public int solution(int[] temp, int k) {
+    public static boolean canPass(int num){
+        int cannot_pass=0;
+        for (int i=0;i<stones.length;i++){
+            if (stones[i]<num){
+                cannot_pass++;
+                if (cannot_pass>=k){ // 연속으로 k개 이상의 돌을 못 밟게 되면 건널 수 없음
+                    return false;
+                }
+            }
+            else{
+                cannot_pass=0; // 밟을 수 있는 돌을 만나면 카운트 리셋            
+            }
+        }
+    return true;
+    }    
+    
+    static int [] stones;
+    static int k;
+    
+    public int solution(int[] temp, int kk) {
+        k=kk;
+        stones=temp;
+        
         int answer = 0;
-        
-        // 맨 앞에 0 삽입 후 원본 배열 카피
-        int[] stones = new int[temp.length + 1];
-        stones[0] = 100;
-        
-        for (int i = 0; i < temp.length; i++) {
-            stones[i + 1] = temp[i];
-        }
-        
-        while (true) {
-            //시작점에서 첫 출발
-            int cur_idx = 0;
-            boolean complete = false;
-            ArrayList<Integer> visited_idx=new ArrayList<>();
-            
-            while (true) {
-                boolean moved = false;
-                
-                // 다음으로 밟을 수 있는 디딤돌 중 가장 가까운 것 선택
-                for (int i = 1; i <= k; i++) {
-                    
-                    // 마지막 돌을 넘어감 → 한 명 건넘
-                    if (cur_idx + i >= stones.length) {
-                        complete = true;
-                        break;
-                    }
-                    
-                    // 갈 수 있는 돌이 있는 경우 -> 해당 돌 -- 해주고 인덱스 이동, 그 인덱스에서 다시 for문 시작 (*)
-                    if (stones[cur_idx + i] != 0) {
-                        stones[cur_idx + i]--;
-                        cur_idx = cur_idx + i;
-                        visited_idx.add(cur_idx);
-                        moved = true;
-                        break;
-                    }
-                }
-                
-                // 이번 사람이 건넘
-                if (complete) {
-                    answer++;
-                    //이 사람이 이동했던 인덱스들이 저장된 arraylist 사용 -> 참조값 중 min 찾아서 그만큼 다 빼줌 
-                    int min_num=Integer.MAX_VALUE;
-                    for (int i=0;i<visited_idx.size();i++){
-                        min_num=stones[visited_idx.get(i)]<min_num?stones[visited_idx.get(i)]:min_num;
-                    }
-                    for (int i=0;i<visited_idx.size();i++){
-                        stones[visited_idx.get(i)]-=min_num;
-                    }
-                    answer+=min_num;
-                    break;
-                }
-                
-                // 아직 건너지 못했는데 k칸 안에 밟을 수 있는 돌이 없는 경우 여기서 종료 
-                if (!moved) {
-                    break;
-                }
-                
-                //(*)는 위의 조건에 해당되지 않으므로 다시 2번째 while(true)로 올라감 
+        int l=0;
+        int r=200000000;
+
+        while(l<=r){
+            int c=(l+r)/2;   //c명의 사람이 건널 수 있는지 봄 
+            if (canPass(c)){
+                answer=c;
+                l=c+1;
+            }else{
+                r=c-1;
             }
-            
-            // 아직 건너지 못했는데 k칸 안에 밟을 수 있는 돌이 없고 이번 사람이 못 건넜으면 종료
-            if (!complete) {
-                break;
-            }
-            
-            //complete가 true면 처음부터 다시 시작 
         }
-        
         return answer;
     }
 }
